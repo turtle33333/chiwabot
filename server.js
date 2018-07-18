6c6d8a3d2a34f3db8f4060c9dbde0e84ad217084
turtle33333 <31820075+turtle33333@users.noreply.github.com>
2018-07-18 11:34:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const client = new AkairoClient({
     disableEveryone: true
 });
 
-client.login('NDY3MzkzNDkzNzQ1MTM5NzMy.DisGCA.xJuPl1tVVyhyx7dxGzQ4tstFRo8').then(() => {
+client.login('TOKEN').then(() => {
   console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity(`c!help`);
         
